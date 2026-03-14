f37065ea23d32b8d9c7dda53c9824af82caf9518
Khalid-Almubarak <69424182+Khalid-Almubarak@users.noreply.github.com>
2026-03-13 23:51:32 -0400

    Added try/except error handling to FileLoader.load()

--- file_loader.py
+++ file_loader.py
@@ -1,6 +1,6 @@
 import json
 from data_loader import DataLoader
-from data_load_state import DataLoadState, DataLoadSuccessState
+from data_load_state import DataLoadState, DataLoadSuccessState, DataLoadErrorState
 
 
 class FileLoader(DataLoader):
@@ -10,6 +10,10 @@ class FileLoader(DataLoader):
         self.file_name = file_name
 
     def load(self) -> DataLoadState:
-        with open(self.file_name, 'r') as f:
-            data = json.load(f)
-        return DataLoadSuccessState(data)
+        try:
+            with open(self.file_name, 'r') as f:
+                data = json.load(f)
+            return DataLoadSuccessState(data)
+        except Exception as e:
+            print(e)
+            return DataLoadErrorState(e)

2996698f05d99c7440428dec8f2cba8f8239cc95
Fayiz-a <68380206+Fayiz-A@users.noreply.github.com>
2026-03-27 22:30:40 -0400

    Removed capital constants from class, removed DRY code, and corrected the implementation of all roads in bound visible if big enough zoom

--- streamlit_manager.py
+++ streamlit_manager.py
@@ -10,7 +10,7 @@ class StreamlitManager:
     _road_manager: RoadManager
     _roads: dict[str, UIRoad]
     _selected_roads: dict[str, UIRoad]
-    AVERAGE_LENGTH: float
+    _road_average_length: float
     _info_display_state: InfoDisplayState
 
     def __init__(self) -> None:
@@ -22,9 +22,9 @@ class StreamlitManager:
             total_length += length
 
         if len(self._roads) > 0:
-            self.AVERAGE_LENGTH = total_length / len(self._roads)
+            self._road_average_length = total_length / len(self._roads)
         else:
-            self.AVERAGE_LENGTH = 0
+            self._road_average_length = 0
 
     def _handle_road_removal(self) -> None:
         pass
@@ -36,41 +36,40 @@ class StreamlitManager:
         """
         Update the visible roads according to the given zoom level, the road will be invisible if all portions are
         not in the current bounds or the road length is too minor to display. If the zoom level given is equal
-        to or greater than 13, all the roads should be visible.
+        to or greater than 13, all the roads should be visible that pass the aforementioned bound check.
 
         Preconditions:
             - 0 <= zoom_level <= constants.MAX_ZOOM
         """
-        if zoom_level >= 13:
-            for ui_road in self._roads.values():
-                ui_road.visible = True
-            return
-
-        length_bound: float = (constants.MAX_ZOOM - zoom_level + 1) * self.AVERAGE_LENGTH
+        length_bound: float = (constants.MAX_ZOOM - zoom_level + 1) * self._road_average_length
 
         northeast_candidate: Coordinate = bounds[0]
         southwest_candidate: Coordinate = bounds[1]
+        longitudes: tuple[float, float] = (northeast_candidate.longitude, southwest_candidate.longitude)
+        latitudes: tuple[float, float] = (northeast_candidate.latitude, southwest_candidate.latitude)
 
-        max_longitude: float = max(northeast_candidate.longitude, southwest_candidate.longitude)
-        min_longitude: float = min(northeast_candidate.longitude, southwest_candidate.longitude)
-        max_latitude: float = max(northeast_candidate.latitude, southwest_candidate.latitude)
-        min_latitude: float = min(northeast_candidate.latitude, southwest_candidate.latitude)
+        max_longitude: float = max(longitudes)
+        min_longitude: float = min(longitudes)
+        max_latitude: float = max(latitudes)
+        min_latitude: float = min(latitudes)
 
         for ui_road in self._roads.values():
             road: Road = ui_road.road
             length: float = road.length
 
-            length_check: bool = length >= length_bound
-            bounds_check: bool = False
+            # ignore how big or small a road is if the user is zoomed in enough, and thus show all roads
+            # that pass the bounds check
+            length_check_passed: bool = zoom_level >= constants.BIG_ENOUGH_ZOOM_THRESHOLD or length >= length_bound
+            bounds_check_passed: bool = False
 
-            if length_check:
+            if length_check_passed:
                 for coord in road.geometry:
                     if (min_latitude <= coord.latitude <= max_latitude and
                             min_longitude <= coord.longitude <= max_longitude):
-                        bounds_check = True
+                        bounds_check_passed = True
                         break
 
-            ui_road.visible = length_check and bounds_check
+            ui_road.visible = length_check_passed and bounds_check_passed
 
     def reset(self) -> None:
         pass
